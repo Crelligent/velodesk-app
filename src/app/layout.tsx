eb8da0f82c56eaab2739972993d6c3a73c5081ce
EnergySmartNigeria <energysmartng@gmail.com>
2026-09-13 08:30:05 +0100

    fix: move apollo tracker to head to match provider instructions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -170,6 +170,12 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Apollo.io Website Tracker */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"6a900322ed90b0001c04ad81"})},document.head.appendChild(o)}initApollo();`
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${outfit.variable} antialiased`}
@@ -188,15 +194,6 @@ export default function RootLayout({
             gtag('js', new Date());
 
             gtag('config', 'G-89LVTFH0GF');
-          `}
-        </Script>
-        {/* Apollo.io Website Tracker */}
-        <Script id="apollo-tracker" strategy="afterInteractive">
-          {`
-            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
-            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-            o.onload=function(){window.trackingFunctions.onLoad({appId:"6a900322ed90b0001c04ad81"})},
-            document.head.appendChild(o)}initApollo();
           `}
         </Script>
       </body>
